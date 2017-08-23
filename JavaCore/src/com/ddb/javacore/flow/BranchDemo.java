1ce486a7ad71526a5007b07d0def0cae09109bf1
lingfeng <1797890817@qq.com>
2017-08-23 10:44:50 +0800

    add Content

--- JavaCore/src/com/ddb/javacore/flow/BranchDemo.java
+++ JavaCore/src/com/ddb/javacore/flow/BranchDemo.java
@@ -23,6 +23,10 @@ public class BranchDemo {
 			System.out.println("小样不及格，等着补考吧！！");
 		}
 
+		// 三目运算符 ? :
+		String result = score > 60 ? "及格" : "不及格";
+		System.out.println("result :" + result);
+
 		// 如果...否则...连接的多路分支;
 		if (score >= 90) {
 			System.out.println("很好，成绩优秀！");
@@ -36,9 +40,47 @@ public class BranchDemo {
 			System.out.println("不及格，等着补考吧！");
 		}
 
-		// 三目运算符 ? :
-		String result = score > 60 ? "及格" : "不及格";
-		System.out.println("result :" + result);
+		// switch...case...的多路分支
+		System.out.println("下面演示switch...case:");
+		switch (score) {
+		case 90:
+			System.out.println("你的成绩是90分，非常好！");
+			break;
+		case 80:
+			System.out.println("你的成绩是80分，很好！");
+			break;
+		case 70:
+			System.out.println("你的成绩是70分，还不错！");
+			break;
+		case 60:
+			System.out.println("你的成绩是60分，及格了！");
+			break;
+		default:
+			System.out.println("杯具了，不及格！");
+			break;
+		}
+		// 总结：多路分支是范围表示，switch...case并不是很适合；
+		// switch...case 适合的是常量，固定的东西。
+		// 多路if其实比switch要强大
+		// 通常情况下，break不可以省略
+		// 默认分支在开始和在结束作用一样。
+
+		// 分支嵌套
+		System.out.println("------------------------------");
+		if (score >= 60) {
+			if (score > 70) {
+				if (score > 80) {
+					System.out.println("您的分数是80分以上");
+				} else {
+					System.out.println("您的分数是在70和80之间！");
+				}
+				
+			} else {
+				System.out.println("您的分数是在60和70之间！");
+			}
+		} else {
+			System.out.println("分支嵌套：考试不及格！！");
+		}
 
 		System.out.println("执行结束！");
 
