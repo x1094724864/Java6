10939b044f89644e2ea99ba226f461d8e2b3ad08
lingfeng <1797890817@qq.com>
2017-08-23 16:56:15 +0800

    demo

--- JavaCore/src/com/ddb/javacore/flow/LoopDemo.java
+++ JavaCore/src/com/ddb/javacore/flow/LoopDemo.java
@@ -40,15 +40,23 @@ public class LoopDemo {
 //		}
 		
 		//计算5门课程的平均分
-//		int sum=0;	//表示5门课程的成绩总和
-//		Scanner input = new Scanner(System.in);
-//		for (int i = 1; i <= 5; i++) {
-//			System.out.println("请输入5门课程的第"+i +"门课程成绩：");
-//			sum += input.nextInt();
-//		}
-//		System.out.println("5门课程的平均分为："+ (sum/5));
-//		int avg = sum / 5;
-//		System.out.println("5门课程的平均分为："+ avg);
+		int sum=0;	//表示5门课程的成绩总和
+		Scanner input = new Scanner(System.in);
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("请输入5门课程的第"+i +"门课程成绩：");
+			int score = input.nextInt();
+			// break;	//没有判断的话，会造成后面的代码为死码！
+			//continue;	//通常情况下都会和判断分支一起用
+			//return;
+//			if(score < 0 || score > 150 ) {
+//				System.out.println("输入成绩非法，程序自动退出！");
+//				return;
+//			}
+			sum += score;
+		}
+		System.out.println("5门课程的平均分为："+ (sum/5));
+		int avg = sum / 5;
+		System.out.println("5门课程的平均分为："+ avg);
 		
 		//多初始值for循环
 //		final int SUM = 6;
@@ -57,9 +65,14 @@ public class LoopDemo {
 //		}
 		
 		// 打印矩形，嵌套循环
-//		for (int i = 0; i <= 3; i++) {
+//		for (int i = 0; i <= 3; i++) {	//打印高
 //			System.out.print("*");
-//			for (int j = 0; j <= 7; j++) {
+//			for (int j = 0; j <= 7; j++) {	//打印长度
+//				if(j==3) {
+//					//break;	// 只能打破它所属的循环，再外层打破不了
+//					//continue;
+//					return;		//return 终止方法的继续执行，所以实现立即终止所有循环，
+//				}
 //				System.out.print("*");
 //			}
 //			System.out.println();
@@ -67,11 +80,20 @@ public class LoopDemo {
 		
 		// 增强for循环 foreach用来遍历数组或者容器
 		//定义一个String类型的数组，含有四个初始元素！
-		String[] array = {"AA","BB","CC","DD"}; 
-		//冒号后面的是容器，冒号前面的是当次循环的元素。当次循环的的元素要有类型的声明
-		for (String string : array) {
-			System.out.println(string);
-		}
+//		String[] array = {"AA","BB","CC","DD"}; 
+//		//冒号后面的是容器，冒号前面的是当次循环的元素。当次循环的的元素要有类型的声明
+//		for (String string : array) {
+//			if(string.equals("BB")) {
+//				//break;	//终止循环
+//				//continue;	//跳过当前，开始执行下一次循环
+//				return;		//不只是跳出循环，还会结束方法的执行
+//			}
+//			System.out.println(string);
+//			
+//		}
+		
+		System.out.println("执行结束！");
+		
 		
 	}
 
