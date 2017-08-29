e0900013d43113f78a0672c39d33cdd9aafab4f2
lingfeng <1797890817@qq.com>
2017-08-29 09:27:04 +0800

    constructor

--- JavaCore/src/com/ddb/javacore/oop2/TestPenguin.java
+++ JavaCore/src/com/ddb/javacore/oop2/TestPenguin.java
@@ -17,17 +17,22 @@ public class TestPenguin {
 //		Penguin pg3 = new Penguin();
 		
 		//使用有构造函数
-		Penguin pg = new Penguin("Q1","Q仔",30);
-		Penguin pg2 = new Penguin("Q2","Q仔",25);
-		Penguin pg3 = new Penguin("Q3","Q仔",20);
+//		Penguin pg = new Penguin("Q1","Q仔",30);
+//		Penguin pg2 = new Penguin("Q2","Q仔",25);
+//		Penguin pg3 = new Penguin("Q3","Q仔",20);
+//		
+//		// 打印pg对象的信息
+//		System.out.println(pg.toString());
+//		System.out.pr;intln(pg2.toString());
+//		System.out.println(pg3.toString());
 		
-		// 打印pg对象的信息
-		System.out.println(pg.toString());
-		System.out.println(pg2.toString());
-		System.out.println(pg3.toString());
+		//静态的属性和方法演示
+//		System.out.println(Penguin.SEX_FEMALE);
+//		System.out.println(Penguin.SEX_MALE);
+//		Penguin.print();
 		
-		
-
+		Penguin penguin = new Penguin("QQ", "Q仔", 45, 3);
+		System.out.println(penguin.toString());
 	}
 
 }
