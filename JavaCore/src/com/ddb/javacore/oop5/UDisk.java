c53d490b812f7ac22a68bd0fdfc3276601535b85
lingfeng <1797890817@qq.com>
2017-08-30 10:10:03 +0800

    add comment

--- JavaCore/src/com/ddb/javacore/oop5/UDisk.java
+++ JavaCore/src/com/ddb/javacore/oop5/UDisk.java
@@ -7,6 +7,7 @@ public class UDisk implements UsbInterface {
 		System.out.println("已经连接USB接口，正在进行数据传输...");
 	}
 
+	//没有在接口中声明，所以通过接口是看不见的
 	public void print() {
 		System.out.println("我是UDisk---优盘！");
 	}
