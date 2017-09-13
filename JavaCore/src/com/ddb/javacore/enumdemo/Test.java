a818d88afa421850db052e6e83e76a05024f5ff0
dbdu <1797890817@qq.com>
2017-09-13 16:11:46 +0800

    add code

--- JavaCore/src/com/ddb/javacore/enumdemo/Test.java
+++ JavaCore/src/com/ddb/javacore/enumdemo/Test.java
@@ -1,9 +1,12 @@
 package com.ddb.javacore.enumdemo;
 
+import java.util.EnumMap;
 import java.util.EnumSet;
+import java.util.Map.Entry;
 
 public class Test {
 
+	@SuppressWarnings("all")
 	public static void main(String[] args) {
 		// 遍历枚举的所有实例
 		// System.out.println("输出WeekEnum:");
@@ -48,9 +51,36 @@ public class Test {
 
 		// EnumSet demo
 		System.out.println("EnumSet :");
-		EnumSet<SeasonEnum> seasonSet = EnumSet.allOf(SeasonEnum.class);	//将枚举实例转换为Set集合的元素
+		EnumSet<SeasonEnum> seasonSet = EnumSet.allOf(SeasonEnum.class); // 将枚举实例转换为Set集合的元素
 		for (SeasonEnum seasonEnum2 : seasonSet) {
 			System.out.println(seasonEnum2);
+		}
+
+		// EnumMap的使用
+		System.out.println("EnumMap :");
+		EnumMap<WeekEnum2, String> weekMap = new EnumMap(WeekEnum2.class);
+		weekMap.put(WeekEnum2.MON, "一");
+		weekMap.put(WeekEnum2.TUE, "二");
+		weekMap.put(WeekEnum2.WED, "三");
+		weekMap.put(WeekEnum2.THU, "四");
+		weekMap.put(WeekEnum2.FRI, "五");
+		weekMap.put(WeekEnum2.SAT, "六");
+		weekMap.put(WeekEnum2.SUN, "日");
+		for (Entry<WeekEnum2, String> entry : weekMap.entrySet()) {
+			System.out.println(entry.getKey().getIndex() + "--" + entry.getKey().getDesc() + "--" + entry.getValue());
+		}
+
+		// 枚举比较
+		WeekEnum2 weekEnum2 = WeekEnum2.MON;
+		int result = weekEnum2.compareTo(WeekEnum2.THU); // 执行枚举间的比较
+		System.out.println("比较的结果：" + result);
+		if (result > 0) {
+			System.out.println("Mon在后面");
+		} else if (result == 0) {
+			System.out.println("Mon位置相同");
+		} else {
+			System.out.println("Mon位置前面");
+
 		}
 
 	}
